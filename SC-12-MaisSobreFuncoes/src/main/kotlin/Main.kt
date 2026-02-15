c3a90babdc17ad6435735ac5ab2dc426307c67f1
Andre <andre.tors448@gmail.com>
2026-02-15 19:53:51 -0300

    SC-12-Concluido

---
+++ SC-12-MaisSobreFuncoes/src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun endereco(rua: String, cidade: String, estado: String, cep: String = ""){
+
+}
+
+fun media( vararg n: Float) : Float {
+    var soma = 0F
+    for(i in n){
+        soma += i
+    }
+
+    return soma / n.size
+}
+
+fun calculaJuros(): Nothing{
+    // logica - implementacao
+    throw Exception()
+}
+
+fun anyExemplo( vararg abc: Any) : Float {
+    var soma = 0F
+    for(i in abc){
+        when(i) {
+            is Int -> print("Int")
+            is Float -> print("Float")
+            is String -> print("String")
+            is Boolean -> print("Boolean")
+            is Char -> print("Char")
+        }
+    }
+
+    return abc.size
+}
+
+fun main(){
+    endereco("rua", estado = "estado", cidade = "cidade")
+
+    media( 8F, 10F, 5F, 5F, 6F, 78F)
+
+    anyExemplo( 8F, 10F, 5F, 5F, 6F, 78F, "", false, 5, 'd')
+
+}
